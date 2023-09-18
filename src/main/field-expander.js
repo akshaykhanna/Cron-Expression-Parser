107a5f38d73bbd0d84ba3e46d84e69edac685d0c
akshaykhanna <akshay9911102163@gmail.com>
2023-09-18 20:02:48 +0530

    Refactor field expander

--- src/main/field-expander.js
+++ src/main/field-expander.js
@@ -1,38 +1,39 @@
-const {fieldsMap } = require('./constants');
+const { fieldsMap } = require('./constants');
 
 module.exports = function (fieldKey, field) {
     if (fieldKey === 'cmd') {
         return [field];
     }
-    const expandedField = [];
     if (field === '*') {
         return _getAllPossibleFields(fieldKey);
     }
-    const parts = field.split(',');
-
-    for (const part of parts) {
-        if (part.includes('-')) {
-            const [start, end] = part.split('-').map(Number);
-            expandedField.push(..._generateFieldBtw(start, end, fieldKey));
-        } else if (part.includes('/')) {
-            let [start, step] = part.split('/');
-            expandedField.push(..._generateFieldBtw(start, fieldsMap[fieldKey].end, fieldKey, Number(step)));
-        } else {
-            expandedField.push(Number(part));
-        }
+    if (field.includes('-')) {
+        const [start, end] = field.split('-').map(Number);
+        return _generateFieldBtw(start, end, fieldKey);
     }
+    if (field.includes('/')) {
+        const [start, step] = field.split('/');
+        return _generateFieldBtw(start, fieldsMap[fieldKey].end, fieldKey, Number(step));
+    }
+    if (field.includes(',')) {
+        return _getCommaSeparatedFields(field);
+    }
+    return [Number(field)];
 
-    return expandedField;
+}
+
+function _getCommaSeparatedFields(field) {
+    return field.split(',').map(Number);
 }
 
 function _getValidStartAndEnd(start, end, fieldKey) {
     let [validStart, validEnd] = [start, end];
-    if(fieldKey && fieldKey in fieldsMap) {
+    if (fieldKey && fieldKey in fieldsMap) {
         const fieldObj = fieldsMap[fieldKey];
         validStart = !start || start === '*' ? fieldObj.start : Number(start);
         validEnd = !end || end === '*' ? fieldObj.end : Number(end);
     }
-    return [ validStart, validEnd ];
+    return [validStart, validEnd];
 }
 
 function _generateFieldBtw(start, end, fieldKey, step = 1) {
